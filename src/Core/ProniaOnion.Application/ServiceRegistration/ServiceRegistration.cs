2ce98511441d33eae7119d42d8dec8a97f77cfc4
Wertdrof <wertdrof@gmail.com>
2025-01-03 20:31:50 +0400

    Validator added

--- src/Core/ProniaOnion.Application/ServiceRegistration/ServiceRegistration.cs
+++ src/Core/ProniaOnion.Application/ServiceRegistration/ServiceRegistration.cs
@@ -1,5 +1,8 @@
 ﻿using System.Reflection;
+using FluentValidation;
+using FluentValidation.AspNetCore;
 using Microsoft.Extensions.DependencyInjection;
+using ProniaOnion.Application.Validators;
 
 namespace ProniaOnion.Application.ServiceRegistration
 {
@@ -8,6 +11,11 @@ namespace ProniaOnion.Application.ServiceRegistration
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddFluentValidationAutoValidation()
+             .AddFluentValidationClientsideAdapters()
+             .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            //???
+            services.AddValidatorsFromAssembly(typeof(CreateCategoryDtoValidator).Assembly);
 
             return services;
         }
---
+++ src/Core/ProniaOnion.Application/Validators/CreateCategoryDtoValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using ProniaOnion.Application.Abstractions.Repositories.Generic;
+using ProniaOnion.Application.DTOs.Categories;
+
+namespace ProniaOnion.Application.Validators
+{
+    public class CreateCategoryDtoValidator : AbstractValidator<CreateCategoryDto>
+    {
+        private readonly ICategoryRepository _repository;
+
+        public CreateCategoryDtoValidator(ICategoryRepository repository)
+        {
+            _repository = repository;
+            for (var i = 0; i < 100; i++)
+            {
+                Console.WriteLine("CreateCategoryDtoValidator created");
+            }
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Data required")
+                .MaximumLength(100).WithMessage("Characters should be less than 100")
+                .Matches(@"^[A-Za-z\s0-9]*$");
+            //  .MustAsync(CheckNameExistence);
+
+        }
+
+        //public async Task<bool> CheckNameExistence(string name, CancellationToken token)
+        //{
+        //    return !await _repository.AnyAsync(c => c.Name == name);
+        //}
+    }
+
+}
